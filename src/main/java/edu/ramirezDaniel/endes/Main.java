a115d3445513ccd3f128c9d99f85dbd9b74662e4
PC\danir <comans.ramirezz@gmail.com>
2026-03-24 17:37:20 +0100

    Codigo Completo

--- src/main/java/edu/ramirezDaniel/endes/Main.java
+++ src/main/java/edu/ramirezDaniel/endes/Main.java
@@ -1,17 +1,45 @@
 package edu.ramirezDaniel.endes;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import edu.ramirezDaniel.endes.entities.Alumno;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Clase principal de pruebas para el proyecto Practica2-ENDES.
+ * Se encarga de verificar el correcto funcionamiento de la clase Alumno,
+ * incluyendo sus constructores, encapsulación y métodos estáticos.
+ * * @author Daniel Ramírez
+ */
+public class Main {
+
+    /**
+     * Punto de entrada principal de la aplicación.
+     * @param args Argumentos de la línea de comandos.
+     */
+    public static void main(String[] args) {
+
+        // 1. Prueba del Constructor por defecto y Setters (Encapsulación)
+        Alumno a1 = new Alumno();
+        a1.setNombre("Daniel");
+        a1.setApellidos("Ramírez");
+        a1.setEdad(19);
+        a1.setNotaMedia(8.5);
+
+        // 2. Prueba del Constructor completo (8 atributos)
+        Alumno a2 = new Alumno(2, "Juan", "Pérez", 17, "juan@gmail.com", "DAW", 4.0, true);
+
+        // 3. Demostración de Getters (Acceso a datos privados)
+        System.out.println("--- DATOS DE LOS ALUMNOS ---");
+        System.out.println("Alumno 1: " + a1.getNombre() + " " + a1.getApellidos() + " - Nota: " + a1.getNotaMedia());
+        System.out.println("Alumno 2: " + a2.getNombre() + " " + a2.getApellidos() + " - Nota: " + a2.getNotaMedia());
+
+        // 4. Pruebas de los Métodos Estáticos (Lógica de negocio)
+        System.out.println("\n--- PRUEBAS DE MÉTODOS ESTÁTICOS ---");
+
+        // Comprobación de mayoría de edad
+        System.out.println("¿Es " + a1.getNombre() + " mayor de edad?: " + Alumno.esMayorDeEdad(a1.getEdad()));
+        System.out.println("¿Es " + a2.getNombre() + " mayor de edad?: " + Alumno.esMayorDeEdad(a2.getEdad()));
+
+        // Obtención de calificación cualitativa
+        System.out.println("Calificación de " + a1.getNombre() + ": " + Alumno.obtenerCalificacionTexto(a1.getNotaMedia()));
+        System.out.println("Calificación de " + a2.getNombre() + ": " + Alumno.obtenerCalificacionTexto(a2.getNotaMedia()));
     }
 }
